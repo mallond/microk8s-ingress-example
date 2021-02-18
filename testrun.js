bb82cbba753cae609d8e5017467e4d45e756edc8
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 15:39:03 -0800

    Update testrun.js

--- testrun.js
+++ testrun.js
@@ -1,70 +1,10 @@
-/*
-    LambdaTest selenium automation sample example
-    Configuration
-    ----------
-    username: Username can be found at automation dashboard
-    accessKey:  AccessKey can be generated from automation dashboard or profile section
- 
-    Result
-    -------
-    Execute NodeJS Automation Tests on LambdaTest Distributed Selenium Grid
-*/
-const webdriver = require('selenium-webdriver');
- 
-/*
-    Setup remote driver
-    Params
-    ----------
-    platform : Supported platform - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
-    browserName : Supported platform - (chrome, firefox, Internet Explorer, MicrosoftEdge, Safari)
-    version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
-*/
- 
-// username: Username can be found at automation dashboard
-const USERNAME = '{username}';
- 
-// AccessKey:  AccessKey can be generated from automation dashboard or profile section
-const KEY = '{accessKey}';
- 
-// gridUrl: gridUrl can be found at automation dashboard
-const GRID_HOST = 'http://10.152.183.186:4444/wd/hub';
- 
-function searchTextOnGoogle() {
-    // Setup Input capabilities
-    const capabilities = {
-        platform: 'windows 10',
-        browserName: 'chrome',
-        version: '67.0',
-        resolution: '1280x800',
-        network: true,
-        visual: true,
-        console: true,
-        video: true,
-        name: 'Test 1', // name of the test
-        build: 'NodeJS build' // name of the build
-    }
- 
-    // URL: https://{username}:{accessKey}@hub.lambdatest.com/wd/hub
-    const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
- 
-    // setup and build selenium driver object
-    const driver = new webdriver.Builder()
-        .usingServer(gridUrl)
-        .withCapabilities(capabilities)
-        .build();
- 
-    // navigate to a url, search for a text and get title of page
-    driver.get('https://www.google.com/ncr').then(function() {
-        driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
-            driver.getTitle().then(function(title) {
-                setTimeout(function() {
-                    console.log(title);
-                    driver.quit();
-                }, 5000);
-            });
-        });
-    });
-}
-searchTextOnGoogle();
+var chrome = require(“selenium-webdriver/chrome”);
 
+var driver = new webdriver.Builder()
+.forBrowser(‘chrome’)
+.usingServer(‘http://10.152.183.186:4444/wd/hub’)
+.build();
 
+driver.get(“https://www.softpost.org”);
+
+driver.quit();ls
