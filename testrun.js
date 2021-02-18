629cfaee9d0a6d18cd63e2bbfa25319d911be80e
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 15:43:50 -0800

    Update testrun.js

--- testrun.js
+++ testrun.js
@@ -1,10 +1,11 @@
-var chrome = require('selenium-webdriver/chrome');
-
+var webdriver = require('selenium-webdriver');
 var driver = new webdriver.Builder()
-.forBrowser('chrome')
-.usingServer('http://10.152.183.186:4444/wd/hub')
-.build();
-
-driver.get('https://www.softpost.org');
-
-driver.quit();ls
+            .usingServer('http://10.152.183.186:4444/wd/hub')
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .build();
+driver.get("http://www.google.com");
+driver.getTitle().then(function(title) {
+  console.log('Page title is: ' + title);
+});
+driver.quit();
+console.log("Done");
