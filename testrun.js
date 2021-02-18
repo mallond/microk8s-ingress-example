ec6a554e034b4a1afa2bd9845572989466d88dd8
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 15:40:19 -0800

    Update testrun.js

--- testrun.js
+++ testrun.js
@@ -1,10 +1,10 @@
-var chrome = require(“selenium-webdriver/chrome”);
+var chrome = require('selenium-webdriver/chrome');
 
 var driver = new webdriver.Builder()
-.forBrowser(‘chrome’)
-.usingServer(‘http://10.152.183.186:4444/wd/hub’)
+.forBrowser('chrome')
+.usingServer('http://10.152.183.186:4444/wd/hub')
 .build();
 
-driver.get(“https://www.softpost.org”);
+driver.get('https://www.softpost.org');
 
 driver.quit();ls
