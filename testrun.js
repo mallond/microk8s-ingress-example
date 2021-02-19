e209ffc8f71ff24e15ff1bd6d80017e4dd7d6068
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 16:07:08 -0800

    Update testrun.js

--- testrun.js
+++ testrun.js
@@ -1,6 +1,6 @@
 var webdriver = require('selenium-webdriver');
 var driver = new webdriver.Builder()
-            .usingServer('http://10.152.183.186:4444/wd/hub')
+            .usingServer('http://10.152.183.229:6900/wd/hub')
             .withCapabilities(webdriver.Capabilities.chrome())
             .build();
 driver.get("http://www.google.com");
