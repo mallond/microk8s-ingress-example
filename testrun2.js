b79cbba114ddedbf8375ce44a3276ddfadf8c99b
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 16:29:57 -0800

    Update testrun2.js

--- testrun2.js
+++ testrun2.js
@@ -1,17 +1,32 @@
 var webdriver = require('selenium-webdriver');
+const GRID_HOST = 'http://10.152.183.186:4444/wd/hub';
 
-var driver = new webdriver.Builder()
-            .usingServer('http://10.152.183.186:4444/wd/hub')
-            .withCapabilities(webdriver.Capabilities.chrome())
-            .build();
-
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.wait(function() {
- return driver.getTitle().then(function(title) {
-   return title === 'webdriver - Google Search';
+const capabilities = {
+    platform: 'windows 10',
+    browserName: 'chrome',
+    version: '67.0',
+    resolution: '1280x800',
+    network: true,
+    visual: true,
+    console: true,
+    video: true,
+    name: 'Test 1', // name of the test
+    build: 'NodeJS build' // name of the build
+}
+    
+const driver = new webdriver.Builder()
+        .usingServer(GRID_HOST)
+        .withCapabilities(capabilities)
+        .build();
+ 
+ // navigate to a url, search for a text and get title of page
+ driver.get('https://www.google.com/ncr').then(function() {
+     driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
+         driver.getTitle().then(function(title) {
+             setTimeout(function() {
+                 console.log(title);
+                 driver.quit();
+             }, 5000);
+         });
+     });
  });
-}, 1000);
-
-driver.quit();
