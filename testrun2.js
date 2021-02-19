8aea98c33a1a9de70be178b783bde1d1cd68e436
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 16:35:53 -0800

    Update testrun2.js

--- testrun2.js
+++ testrun2.js
@@ -1,25 +1,41 @@
-var webdriver = require('selenium-webdriver');
+
 const GRID_HOST = 'http://10.152.183.186:4444/wd/hub';
 
+const webdriver = require('selenium-webdriver');
+// Input capabilities
 const capabilities = {
-    browserName: 'chrome',
-    name: 'Test 1', // name of the test
-    build: 'NodeJS build' // name of the build
+ 'os_version' : '10',
+ 'resolution' : '1920x1080',
+ 'browserName' : 'Chrome',
+ 'browser_version' : 'latest',
+ 'os' : 'Windows',
+ 'name': 'BStack-[NodeJS] Sample Test', // test name
+ 'build': 'BStack Build Number 1', // CI/CD job or build name
+ 'browserstack.user' : 'YOUR_USERNAME',
+ 'browserstack.key' : 'YOUR_ACCESS_KEY'
 }
-    
-const driver = new webdriver.Builder()
-        .usingServer(GRID_HOST)
-        .withCapabilities(capabilities)
-        .build();
- 
- // navigate to a url, search for a text and get title of page
- driver.get('https://www.google.com/ncr').then(function() {
-     driver.findElement(webdriver.By.name('q')).sendKeys('LambdaTest\n').then(function() {
-         driver.getTitle().then(function(title) {
-             setTimeout(function() {
-                 console.log(title);
-                 driver.quit();
-             }, 5000);
-         });
-     });
- });
+async function runSampleTest () {
+  let driver;
+  try {
+    driver = new webdriver.Builder().
+      usingServer(GRID_HOST).
+      withCapabilities(capabilities).build();
+    await driver.get('http://www.google.com');
+    await (await driver.findElement(webdriver.By.name('q'))).sendKeys('BrowserStack\n');
+    const title = await driver.getTitle();
+    console.log(title);
+    // Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page included 'BrowserStack'
+    if(title.includes('BrowserStack')) {
+      await driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains BrowserStack!"}}');
+    } else {
+      await driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Title does not contain BrowserStack!"}}');
+    }
+  } catch (e) {
+    console.log(e);
+  } finally {
+    if(driver) {
+      await driver.quit();
+    }
+  }
+}
+runSampleTest(); 
