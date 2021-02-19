142f7b9284561e04af3ea67677d175cd25486548
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 16:41:34 -0800

    Update testrun2.js

--- testrun2.js
+++ testrun2.js
@@ -21,17 +21,8 @@ async function runSampleTest () {
       usingServer(GRID_HOST).
       withCapabilities(capabilities).build();
     await driver.get('http://www.google.com');
-    await (await driver.findElement(webdriver.By.name('q'))).sendKeys('BrowserStack\n');
-    const title = await driver.getTitle();
-    console.log(title);
-    // Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page included 'BrowserStack'
-    if(title.includes('BrowserStack')) {
-      await driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Title contains BrowserStack!"}}');
-    } else {
-      await driver.executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Title does not contain BrowserStack!"}}');
-    }
   } catch (e) {
-    console.log(e);
+    console.log('from the code',e);
   } finally {
     if(driver) {
       await driver.quit();
