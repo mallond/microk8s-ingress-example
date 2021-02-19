9d1c69662253bebbc23245477e8ddc3740a77fd2
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 16:51:54 -0800

    Update testrun2.js

--- testrun2.js
+++ testrun2.js
@@ -1,26 +1,17 @@
 
 const GRID_HOST = 'http://10.152.183.186:4444/wd/hub';
 
-const webdriver = require('selenium-webdriver');
-// Input capabilities
-const capabilities = {
- 'browserName' : 'Chrome',
- 'name': 'BStack-[NodeJS] Sample Test', // test name
- 'build': 'BStack Build Number 1', // CI/CD job or build name
-}
-async function runSampleTest () {
-  let driver;
-  try {
-    driver = new webdriver.Builder().
-      usingServer(GRID_HOST).
-      withCapabilities(capabilities).build();
-    await driver.get('http://www.google.com');
-  } catch (e) {
-    console.log('from the code',e);
-  } finally {
-    if(driver) {
-      await driver.quit();
+
+const { Builder, Capabilities } = require("selenium-webdriver");
+var capabilities = Capabilities.firefox();
+(async function helloSelenium() {
+    let driver = new Builder()
+        .usingServer(GRID_HOST)   
+        .withCapabilities(capabilities)
+        .build();
+    try {
+        await driver.get('http://www.google.com');
+    } finally {
+        await driver.quit();
     }
-  }
-}
-runSampleTest(); 
+})();  
