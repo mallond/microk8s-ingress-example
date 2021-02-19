93023a8b991e3b4c3c188a8f1b08cf9126774bc5
David Mallon ⚙ <mallond@gmail.com>
2021-02-18 16:42:43 -0800

    Update testrun2.js

--- testrun2.js
+++ testrun2.js
@@ -4,15 +4,9 @@ const GRID_HOST = 'http://10.152.183.186:4444/wd/hub';
 const webdriver = require('selenium-webdriver');
 // Input capabilities
 const capabilities = {
- 'os_version' : '10',
- 'resolution' : '1920x1080',
  'browserName' : 'Chrome',
- 'browser_version' : 'latest',
- 'os' : 'Linux',
  'name': 'BStack-[NodeJS] Sample Test', // test name
  'build': 'BStack Build Number 1', // CI/CD job or build name
- 'browserstack.user' : 'YOUR_USERNAME',
- 'browserstack.key' : 'YOUR_ACCESS_KEY'
 }
 async function runSampleTest () {
   let driver;
